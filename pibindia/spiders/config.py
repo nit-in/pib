f2475b7f6fd0bac8152d96c8fbe75b1b4f4cf4d7
nit-in <nit_in@live.com>
2025-10-27 17:26:49 +0530

    adding config file

---
+++ pibindia/spiders/config.py
@@ -0,0 +1,7 @@
+# config file
+
+PIB_BASE_DIR = "~/pib"
+PIB_ARTICLES_DIR = "~/pib/articles"
+PIB_LINKS_DIR = "~/pib/article_links"
+PIB_TEXT_DIR = "~/pib/article_text"
+PIB_JSON_DIR = "~/pib/json_dir"
